10aaeb621f7a618c413e58ef6274ec9823e5998c
Lucas Antonio <lucasantonio2063@gmail.com>
2023-11-20 11:56:12 -0300

    home page implemented

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello World!"
+    return render_template('logon.html')
+
+@app.route('/topics')
+def topics():
+    return render_template('topics.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
